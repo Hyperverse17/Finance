3a0db7fbcf96a46b2c7d2334c9a9f0c18cf8c67f
Otelo Galicia <ogalicia@vepormas.com>
2024-08-22 10:15:41 -0600

    Mejoras sustanciales: Se pasan varias operaciones al properties y se crea condicion para error general

--- finance/properties.py
+++ finance/properties.py
@@ -1,15 +1,18 @@
 from datetime import date
+############### Operativos ############
 zero         = 0
 one          = 1
-#######################################
-total        = 4000
-daysDuration = 14
-dailyBudget  = 250 #round(total/daysDuration,2)
-########## Calculo de fechas ###########
-paymentDay  = date(2024,8,12)  # Tipo date
-today       = date.today()     # Tipo date
-deltaDays   = today-paymentDay # Tipo date
-elapsedDays = deltaDays.days # el atributo .days devuelve un entero
-######################################
+totalBudget  = 4000
+########## Calculo con fechas ###########
+paymentDay    = date(2024,8,12)  # Fecha de pago [Tipo date]
+nextPayDay    = date(2024,8,26)  # Proxima fecha de pago
+today         = date.today()     # Fecha de hoy
+deltaDays1    = today-paymentDay # Diferencia entre fechas [Tipo Date]
+deltaDays2    = nextPayDay-paymentDay
+elapsedDays   = deltaDays1.days # el atributo .days devuelve un entero operable
+daysDuration  = deltaDays2.days
+remainingDays = daysDuration-elapsedDays
+currDay       = elapsedDays + one
+dailyBudget   = 250 #round(totalBudget/daysDuration,2)
+################ Formato ######################
 sStars = "**************"
-
--- finance/todaysBalance.py
+++ finance/todaysBalance.py
@@ -3,40 +3,31 @@ from properties import *
 
 # Nu debit + bx+ + yay + efectivo
 print()
-currentAmount = int(input("Saldo actual : ")) # 
-currDay       = elapsedDays + one
-shouldAmount  = total-(dailyBudget*elapsedDays)
-difference    = currentAmount-shouldAmount
-
+print(sStars + " Calculos del dia " + str(currDay) +" ["+ str(today) + "] " + sStars)
 print()
-print(sStars + " Calculos del dia " + str(currDay) + " " + sStars)
-print("Presupuesto diario : ${:,.2f}".format(dailyBudget))
-print("Dias transcurridos : " + str(elapsedDays))
-print()
-print("Deberias tener     : ${:,.2f}".format(shouldAmount))
-print("Tienes             : ${:,.2f}".format(currentAmount))
-print()
-
-# print("Actual  : $ {:,.2f}".format(GrandTotal) + sMessage2)
-
-daysDiff = daysDuration-elapsedDays
-
-if daysDiff > one:
-    if difference > zero:
-        print("Felicidades, hoy puedes gastar tus ${:,.2f}".format(dailyBudget) + " diarios mas ${:,.2f}".format(difference))        
-    elif difference == zero:
-        print("Vas bien, hoy solo puedes gastar tu presupuesto diario: ${:,.2f}".format(dailyBudget))
-    elif difference < zero:
-        currentDaily = (dailyBudget + difference)
-        if currentDaily > zero:
-            print("Cuidado, hoy solo tienes: ${:,.2f}".format(currentDaily))
-        elif currentDaily <= zero:
-            print("No gastes nada!")
-
-elif daysDiff == one:
-     print("Llegaste al final, hoy puedes gastar: ${:,.2f}".format(currentAmount))
-
-elif daysDiff < one:
-     print("Error")
-
+if remainingDays >= one:
+    currentAmount = float(input("Saldo actual       : "))
+    shouldAmount  = totalBudget-(dailyBudget*elapsedDays)
+    difference    = currentAmount-shouldAmount
+    print("Dias transcurridos : " + str(elapsedDays))
+    print()
+    print("Presupuesto diario : ${:,.2f}".format(dailyBudget))
+    print("Deberias tener     : ${:,.2f}".format(shouldAmount))
+    print("Tienes             : ${:,.2f}".format(currentAmount))
+    print()
+    if remainingDays > one:
+        if difference > zero:
+            print("Felicidades, hoy puedes gastar tus ${:,.2f}".format(dailyBudget) + " diarios mas ${:,.2f}".format(difference))        
+        elif difference == zero:
+            print("Vas bien, hoy solo puedes gastar tu presupuesto diario: ${:,.2f}".format(dailyBudget))
+        elif difference < zero:
+            currentDaily = (dailyBudget + difference)
+            if currentDaily > zero:
+                print("Cuidado, hoy solo tienes: ${:,.2f}".format(currentDaily))
+            elif currentDaily <= zero:
+                print("Mejor no gastes nada!")
+    elif remainingDays == one:
+        print("Llegaste al final, hoy puedes gastar: ${:,.2f}".format(currentAmount))
+else:
+     print("Error: Actualizar fecha de pago y fecha de proximo pago")
 print()
