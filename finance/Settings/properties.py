0dc792f6968b9dc1524403b78686f4eb5515b40e
Otelo Galicia (Bx+) <ogalicia@vepormas.com>
2024-08-26 09:32:26 -0600

    Se actualiza fecha de pago y se hacen algunos ajustes en el flujo

--- finance/Settings/properties.py
+++ finance/Settings/properties.py
@@ -6,8 +6,8 @@ zero         = 0
 one          = 1
 totalBudget  = 4000
 ########## Calculo con fechas ###########
-paymentDay    = date(2024,8,12)  # Fecha de pago [Tipo date]
-nextPayDay    = date(2024,8,26)  # Proxima fecha de pago
+paymentDay    = date(2024,8,26)  # Fecha de pago [Tipo date]
+nextPayDay    = date(2024,9,11)  # Proxima fecha de pago
 today         = date.today()     # Fecha de hoy
 deltaDays1    = today-paymentDay # Diferencia entre fechas [Tipo Date]
 deltaDays2    = nextPayDay-paymentDay
@@ -15,7 +15,7 @@ elapsedDays   = deltaDays1.days # el atributo .days devuelve un entero operable
 daysDuration  = deltaDays2.days
 remainingDays = daysDuration-elapsedDays
 currDay       = elapsedDays + one
-dailyBudget   = 250 #round(totalBudget/daysDuration,2)
+dailyBudget   = round(totalBudget/daysDuration,2)
 ################ Formato ######################
 sStars = "**************"
 
