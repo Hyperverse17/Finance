ada9a114ae4ad207b34abbea37492e88189b92d0
Hyperverse <jovanigalicia17@gmail.com>
2025-07-27 11:16:15 -0600

    Calculadora para ajustar inversiones siguiendo regla predefinida

--- finance/adjustement.py
+++ finance/adjustement.py
@@ -14,31 +14,55 @@ try:
 
         currVariable = float(input("Ingresa el monto de Renta Variable : "))
         currFixed    = float(input("Ingresa el monto de Renta Fija     : "))
+        toAdd        = float(input("Ingresa cuanto deseas agregar      : "))
+        
         os.system("cls")
-        grandTotal   = currVariable + currFixed
+        investTotal  = currVariable + currFixed
+        #grandTotal   = investTotal + toAdd
         
-        shldVariable = grandTotal*(variablePer/100)
-        shldFixed    = grandTotal*(fixedPer/100)
+        shldVariable = investTotal*(variablePer/100)
+        shldFixed    = investTotal*(fixedPer/100)
         
-        fixedDiff    = shldFixed-currFixed
         variableDiff = shldVariable-currVariable
+        fixedDiff    = shldFixed-currFixed
+            
+        toAddVariable = toAdd*(variablePer/100)
+        toAddFixed    = toAdd*(fixedPer/100)
+
+        toInvestVar   = toAddVariable + variableDiff
+        toInvestFixed = toAddFixed + fixedDiff 
+
+        #grandVariable = grandTotal*(variablePer/100)
+        #grandFixed    = grandTotal*(fixedPer/100)
 
         print(f"Se usa regla del {investRule}")
-        print(f"Total de tus inversiones: ${grandTotal:,.2f}")
+        print(f"Total de tus inversiones: ${investTotal:,.2f}")
         print(f"Ajustando {variablePer}% en Renta Variable y {fixedPer}% en Renta Fija ...")
         print()
         
         time.sleep(1)
 
-        if fixedDiff < 0 and variableDiff > 0:
-            print(f"Retira {fixedDiff:,.2f} de renta fija y pasalos a renta variable")
+        if toInvestVar <= 0 and toInvestFixed > 0:
+            print(f"Atencion en Renta Fija")
+            if toAdd <= shldFixed:
+                toInvestFixed = toAdd
+                toInvestVar   = 0
 
-        elif fixedDiff > 0 and variableDiff < 0:
-            print(f"Retira {variableDiff:,.2f} de renta variable y pasalos a renta fija")
+            elif toAdd > shldFixed:
+                toInvestFixed = shldFixed
+                toInvestVar   = toAdd - shldFixed
 
-        else:
-            print("Tu portafolios esta perfectamente balanceado")
+        elif toInvestVar > 0 and toInvestFixed <= 0:
+            print("Atencion en Renta Variable")
+            if toAdd <= shldVariable:
+                toInvestVar   = toAdd
+                toInvestFixed = 0
 
+            elif toAdd > shldVariable:
+                toInvestVar   = shldVariable
+                toInvestFixed = toAdd - shldVariable
+            
+        print(f"A invertir (v/f)  : {toInvestVar:,.2f}, {toInvestFixed:,.2f}")
 
         goAhead = WantToRepeat(goAhead)
 
