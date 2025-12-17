8fb62ec78acf9a0ac4d45c31c0b1c03c3d55d26b
Hyperverse <jovanigalicia17@gmail.com>
2025-12-17 16:35:13 -0600

    Se cambian funciones de obtencion de investor y user a functions.py

--- finance/core/functions.py
+++ finance/core/functions.py
@@ -2,9 +2,10 @@ import os
 import functools
 import calendar
 import csv
-from finance.core.properties import *
 from typing import Union
 from datetime import datetime, date
+from finance.models.classes import Investor, User
+from finance.core.properties import *
 from finance.core.db import get_connection
 
 conn   = get_connection()
@@ -419,4 +420,46 @@ def updateDates():
         message = f"Informacion actualizada correctamente!"
 
         return message
+    
+@functionLog
+def rowToUser(user_row) -> User:
+    """Instancia un objeto de User a partir de un select"""
+    user = User(id=user_row[0], name=user_row[1], last_name=user_row[2], birthday=user_row[3], gender=user_row[4], email=user_row[5])
+    return user
+
+@functionLog
+def getUserById(user_id:int) -> User | None:
+    """Hace select a users"""
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT id, name, last_name, birthday, gender, email, active FROM users WHERE id = ?""",(user_id,),)
+    user_row = cursor.fetchone()
+    conn.close()
+    if user_row is None:
+        return None
+    else:
+        return rowToUser(user_row)
+
+@functionLog
+def rowToInvestor(investor_row) -> Investor:
+    """Instancia un objeto de Investor a partir de un select"""
+    user_id = investor_row[0]
+    user    = getUserById(user_id)
+    investor = Investor(user, nickname=investor_row[1], investment_rule=investor_row[2], monthly_expenses=investor_row[3], emergency_fund=investor_row[4], variable_amt=investor_row[5], fixed_amt=investor_row[6])
+    return investor
+
+@functionLog
+def getInvestorById(investor_id: int) -> Investor | None:
+    """Hace select a Investors """
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT user_id, nickname, investment_rule, monthly_expenses, emergency_fund, variable_amt, fixed_amt FROM investors WHERE id = ?""",(investor_id,),)
+    investor_row = cursor.fetchone()
+    conn.close()
+
+    if investor_row is None:
+        return None
+    else:
+        return rowToInvestor(investor_row)
+    
 
--- tests/basicTest.py
+++ tests/basicTest.py
@@ -1,57 +1,26 @@
-from datetime import date
-from finance.core.db import get_connection
-from finance.models.classes import User, Investor
+from finance.core.functions import getInvestorById, getUserById
 
 print("\n--- Test de clases y funciones ---\n")
 
-def rowToUser(user_row) -> User:
-    """Instancia un objeto de User a partir de un select"""
-    user = User(id=user_row[0], name=user_row[1], last_name=user_row[2], birthday=user_row[3], gender=user_row[4], email=user_row[5])
-    return user
 
-def getUserById(user_id:int) -> User:
-    """Hace select a users"""
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""SELECT id, name, last_name, birthday, gender, email, active FROM users WHERE id = ?""",(user_id,),)
-    user_row = cursor.fetchone()
-    conn.close()
-    if user_row is None:
-        return None
-    else:
-        return rowToUser(user_row)
 
-def rowToInvestor(investor_row) -> Investor:
-    """Instancia un objeto de Investor a partir de un select"""
-    user_id = investor_row[0]
-    user    = getUserById(user_id)
-    investor = Investor(user, nickname=investor_row[1], investment_rule=investor_row[2], monthly_expenses=investor_row[3], emergency_fund=investor_row[4], variable_amt=investor_row[5], fixed_amt=investor_row[6])
-    return investor
+id = int(input("\nIngresa un id: "))
 
-def getInvestorById(investor_id: int) -> Investor | None:
-    """Hace select a Investors """
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""SELECT user_id, nickname, investment_rule, monthly_expenses, emergency_fund, variable_amt, fixed_amt FROM investors WHERE id = ?""",(investor_id,),)
-    investor_row = cursor.fetchone()
-    conn.close()
+user = getUserById(id)
+print(user.name)
+print(user.last_name)
+print(user.age)
 
-    if investor_row is None:
-        return None
-    else:
-        return rowToInvestor(investor_row)
-    
-id = int(input("\nIngresa un id de inversor: "))
 investor = getInvestorById(id)
+if investor != None:
+    print(investor.name)
+    print(investor.last_name)
+    print(investor.birthday)
+    print(investor.email)
+    print(investor.age)
+    print(investor.gender)
 
-print(investor.name)
-print(investor.last_name)
-print(investor.birthday)
-print(investor.email)
-print(investor.age)
-print(investor.gender)
-
-print(investor.nickname)
-print(investor.investment_rule)
-print(investor.total_portfolio)
-print(investor.last_update)
+    print(investor.nickname)
+    print(investor.investment_rule)
+    print(investor.total_portfolio)
+    print(investor.last_update)
